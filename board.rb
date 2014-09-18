7aec85b2aae5e0ff5acb9f649537efce3371f1f3
David Rozenberg <daveroze@gmail.com>
2014-09-17 23:59:16 -0400

    Create initial board, piece classes

---
+++ board.rb
@@ -0,0 +1,33 @@
+class Board
+  
+  attr_reader :grid    
+  
+  def initialize(setup = true)
+    @grid = create_grid(setup)
+  end
+  
+  protected 
+  
+  def [](position)
+    @grid[position[0]][position[1]]
+  end
+  
+  def []=(position, piece)
+    i, j = position
+    @grid[i][j] = piece
+  end
+  
+  def fill_row(color)
+    fill_rows = (color == :black) ? [0, 1, 2] : [5, 6, 7]  
+  end
+  
+  def create_grid(setup)
+    grid = Array.new(8) { Array.new(8) }
+    return grid unless setup
+    
+    [:red, :black].each do |color|
+      fill_row(color)
+    end
+  end
+  
+end
---
+++ piece.rb
@@ -0,0 +1,34 @@
+# utf - 8
+
+class Piece
+  
+  attr_reader :color, :position, :king, :board
+  
+  def initialize(color, position, board)
+    @color = color
+    @position = position
+    @king = false
+    @board = board
+    
+    board[position] = self  
+  end
+  
+  def perform_slide
+  end
+  
+  def perform_jump
+  end
+  
+  protected
+  
+  def move_diffs
+    king ? [1, -1] : [1]
+  end
+  
+  def symbol
+    color == :black ? ⬤ ⭕
+
+
+  end
+  
+end
