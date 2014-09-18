083a4618b4be04899d57d16ff1c346de50f3fffb
App Academy Student <student@appacademy.io>
2014-09-18 14:26:57 -0400

    Basic pieces movement complete

--- board.rb
+++ board.rb
@@ -3,10 +3,27 @@ require_relative 'piece'
 
 class Board
 
+  SLIDES = [[ 1, -1 ], [ 1, 1 ], [ -1, -1 ], [ -1, 1 ]]
+  JUMPS  = [[ 2, -2 ], [ 2, 2 ], [ -2, -2 ], [ -2, 2 ]]
+
   attr_reader :grid
 
   def initialize(setup = true)
     create_grid(setup)
+  end
+
+  def move(start_pos, end_pos)
+    piece = self[start_pos]
+    #is there a piece at position?
+    raise "There is no piece at this start position" if piece.nil?
+    #determine if slide or jump, check_move_legality
+    move_type = slide_or_jump?(start_pos, end_pos)
+
+    if (move_type == :slide) ? piece.slide(end_pos) : piece.jump(end_pos)
+        make_move(start_pos, end_pos, move_type) #make the move
+    else
+      raise "Your piece can't move like that"
+    end
   end
 
   def [](position)
@@ -19,13 +36,20 @@ class Board
     @grid[i][j] = piece unless @grid[i] == nil
   end
 
+  def middle(start_pos, end_pos)
+    x, y = start_pos
+    i, j = end_pos
+
+    [(x + i)/2, (y + j)/2]
+  end
+
   protected
 
   def to_s
-    rows_string = "   " + ('a'..'h').to_a.join("  ") + "\n"
+    rows_string = "   " + ('0'..'7').to_a.join("  ") + "\n"
 
     @grid.each_with_index do |row, row_num|
-      rows_string += "#{row_num + 1}  " + row.map do |piece|
+      rows_string += "#{row_num}  " + row.map do |piece|
         piece.nil? ? "-" : piece.symbol
       end.join("  ") + "\n"
     end
@@ -34,6 +58,28 @@ class Board
 
   def inspect
     nil
+  end
+
+  def slide_or_jump?(start_pos, end_pos)
+    x, y = start_pos
+    i, j = end_pos
+    step = [i - x, j - y]
+
+    return :slide if SLIDES.include?(step)
+    return :jump if JUMPS.include?(step)
+    raise "This is neither a slide nor a jump."
+
+  end
+
+  def make_move(start_pos, end_pos, move_type)
+    piece = self[start_pos]
+
+    self[start_pos] = nil
+    self[end_pos] = piece
+    self[middle(start_pos, end_pos)] = nil if move_type == :jump
+
+    piece.position = end_pos
+
   end
 
   def fill_row(color)
--- piece.rb
+++ piece.rb
@@ -2,7 +2,12 @@
 
 class Piece
 
-  attr_reader :color, :position, :king, :board
+  attr_reader :color, :king, :board
+  attr_accessor :position
+
+  MOVES_HASH = { :black => [[  1, -1 ], [  1, 1 ], [  2, -2 ], [  2, 2 ]],
+                 :red => [[ -1, -1 ], [ -1, 1 ], [ -2, -2 ], [ -2, 2 ]]
+               }
 
   def initialize(position, color, board)
     @position = position
@@ -13,10 +18,37 @@ class Piece
     board[position] = self
   end
 
-  def perform_slide
+  def slide(end_pos) #check legality, return boolean
+    #return all possible sliding steps (color dependant)
+    ##puts color
+    ##puts "legal_steps: #{legal_steps}"
+    steps = legal_steps.select { |step| step.include?(1) || step.include?(-1) }
+    end_positions = steps.map { |step| try_step(position, step) }
+    #slide is legal if there is no piece of either color to the diagonal
+    legal_end_positions = end_positions.select { |end_pos| board[end_pos].nil? }
+    ##puts "legal slides: #{end_positions}"
+    #check if end_pos is one of the legal steps
+    legal_end_positions.include?(end_pos)
   end
 
-  def perform_jump
+  def jump(end_pos) #check legality, return boolean
+    ##puts color
+    ##puts "possible_steps: #{legal_steps}"
+    #return all possible jumping steps (color dependant)
+    steps = legal_steps.select { |step| step.include?(2) || step.include?(-2) }
+    ##puts "jumps: #{steps}"
+    end_positions = steps.map { |step| try_step(position, step) }
+    ##puts "possible end_positions: #{end_positions}"
+    #legal jump if no piece at the end_pos and different color piece in middle
+    legal_end_positions = end_positions.select do |end_pos|
+      piece_at_end_pos = board[end_pos]
+      piece_in_middle = board[board.middle(position, end_pos)]
+      piece_at_end_pos.nil? && !piece_in_middle.nil? &&
+      piece_in_middle.color != self.color
+    end
+    ##puts "legal end postions: #{legal_end_positions}"
+    #check if end_pos is one of the legal steps
+    legal_end_positions.include?(end_pos)
   end
 
   def symbol
@@ -25,8 +57,18 @@ class Piece
 
   protected
 
-  def move_diffs
-    king ? [1, -1] : [1]
+  def legal_steps
+    king ? MOVES_HASH[color] + MOVES_HASH[other_color] : MOVES_HASH[color]
+  end
+
+  def try_step(start_pos, step)
+    x, y = start_pos
+    i, j = step
+    [x + i, y + j]
+  end
+
+  def other_color
+    color == :black ? :white : :black
   end
 
 
