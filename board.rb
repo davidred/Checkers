b1fd1155fa89abd4b6f0ec729aff22d055e5c32d
App Academy Student <student@appacademy.io>
2014-09-18 10:35:17 -0400

    Setup new board with pieces and display in terminal

--- board.rb
+++ board.rb
@@ -1,41 +1,61 @@
+require 'debugger'
+require_relative 'piece'
+
 class Board
-  
-  attr_reader :grid    
-  
+
+  attr_reader :grid
+
   def initialize(setup = true)
-    @grid = create_grid(setup)
+    create_grid(setup)
   end
-  
-  protected 
-  
-  def to_s
-    rows = ""
-    @grid.each_with_index do |row, row_num|
-      row += "#{row_num}  " + row.map do |square|
-        square.nil? ? "-" : square.symbol
-      
-  end
-  
+
   def [](position)
-    @grid[position[0]][position[1]]
+    i, j = position
+    @grid[i][j] unless @grid[i] == nil
   end
-  
+
   def []=(position, piece)
     i, j = position
-    @grid[i][j] = piece
+    @grid[i][j] = piece unless @grid[i] == nil
   end
-  
+
+  protected
+
+  def to_s
+    rows_string = "   " + ('a'..'h').to_a.join("  ") + "\n"
+
+    @grid.each_with_index do |row, row_num|
+      rows_string += "#{row_num + 1}  " + row.map do |piece|
+        piece.nil? ? "-" : piece.symbol
+      end.join("  ") + "\n"
+    end
+    rows_string
+  end
+
+  def inspect
+    nil
+  end
+
   def fill_row(color)
-    fill_rows = (color == :black) ? [0, 1, 2] : [5, 6, 7]  
+    #debugger
+    fill_rows = (color == :black) ? [0, 1, 2] : [5, 6, 7]
+
+    fill_rows.each do |row|
+      if row % 2 == 0
+        1.step(7,2).each { |column| Piece.new( [row, column], color, self ) }
+      else
+        0.step(6,2).each { |column| Piece.new( [row, column], color, self ) }
+      end
+    end
   end
-  
+
   def create_grid(setup)
-    grid = Array.new(8) { Array.new(8) }
-    return grid unless setup
-    
+    @grid = Array.new(8) { Array.new(8) }
+    return unless setup
+
     [:red, :black].each do |color|
       fill_row(color)
     end
   end
-  
+
 end
--- piece.rb
+++ piece.rb
@@ -1,34 +1,34 @@
 # coding: utf-8
 
 class Piece
-  
+
   attr_reader :color, :position, :king, :board
-  
-  def initialize(color, position, board)
-    @color = color
+
+  def initialize(position, color, board)
     @position = position
+    @color = color
     @king = false
     @board = board
-    
-    board[position] = self  
+
+    board[position] = self
   end
-  
+
   def perform_slide
   end
-  
+
   def perform_jump
   end
-  
+
+  def symbol
+    color == :black ? "●" : "○"
+  end
+
   protected
-  
+
   def move_diffs
     king ? [1, -1] : [1]
   end
-  
-  def symbol
-    color == :black ? 
 
 
-  end
-  
+
 end
